cb8a95a4ebd33c46dca5346e8efaf0b06778ef7b
Ravi <ravi.dhiman@daffodilsw.com>
2020-09-15 23:01:07 +0530

    intent added for share image

--- android/app/src/main/java/com/tranzact/MainActivity.java
+++ android/app/src/main/java/com/tranzact/MainActivity.java
@@ -1,6 +1,12 @@
 package com.tranzact;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.net.Uri;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +17,36 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Tranzact";
+  }
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+      @Override
+      protected Bundle getLaunchOptions() {
+        String value = "";
+        String type = "";
+        String action = "";
+        Intent intent = MainActivity.this.getIntent();
+        action = intent.getAction();
+        type = intent.getType();
+        if (type == null) {
+          type = "";
+        }
+         if (Intent.ACTION_SEND.equals(action) && ("image/*".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type) ) ) {
+          Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+          value = uri.toString();
+        } else {
+          value = "";
+        }
+        Bundle bundle = new Bundle();
+        bundle.putString("type", type);
+        bundle.putString("value",value);
+        return bundle;
+      }
+    };
   }
 }
